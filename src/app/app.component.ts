74f078bce7379d4b919031b31b34a7c383ab62f4
NicklasHellion <nicola.raduazzo@gmail.com>
2022-04-11 09:59:12 +0200

    progetto settimana 11

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit} from '@angular/core';
+import { MovieService } from './components/movies/movies.service';
+@Component({
+  selector: 'app-root',
+  template: `
+  <app-navbar></app-navbar>
+  <router-outlet></router-outlet>
+  `,
+  styles: []
+})
+export class AppComponent {
+
+  constructor(){}
+
+
+}
+
+
---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AuthModule } from './auth/auth.module';
+import { HttpClientModule } from '@angular/common/http';
+
+
+
+
+
+@NgModule({
+  declarations: [AppComponent, NavbarComponent],
+  imports: [BrowserModule, AppRoutingModule, AuthModule, HttpClientModule],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
